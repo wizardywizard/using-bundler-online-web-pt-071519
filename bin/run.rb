0b5cbba5fb2df32e43b7fed38a67529a0364b4da
David Little <littledavid175@gmail.com>
2019-08-12 17:15:03 +0000

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,50 @@
-Hashie
-Sinatra
-Octokit
-Pry
-AwesomePrint
+ require_relative '../config/environment'
+ Hashie
+ Sinatra
+ Octokit
+ Pry
+ AwesomePrint
+
+
+
+#Store has many items
+# class Store 
+  
+#   attr_reader :name
+#   attr_accessor :items
+  
+#   @@shelf = []
+  
+#   def initialize(name)
+#     @name = name
+#   end
+  
+# end
+
+# class Items
+  
+#   attr_accessor :catagory
+  
+# end
+
+# target = Store.new('Target')
+# puts target.name
+# #target.name = 'new_name'
+# puts target
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
